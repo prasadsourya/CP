00c9f5d44f82469f5a8ea39bc84f18ace1f178f2
prasadsourya <prasadsourya@gmail.com>
2020-07-15 21:00:09 +0530

    Jul 15  21:00:09

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -9,4 +9,4 @@ def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	s1=math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
 	s2=math.sqrt((x2-x3)*(x2-x3)+(y3-y2)*(y3-y2))
 	s3=math.sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3))
-	s=s1+s2+s3/2
+	s=(s1+s2+s3)/2
