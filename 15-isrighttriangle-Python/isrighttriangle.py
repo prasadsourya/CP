8dd10ef1d5784399e2cf93111403ab03eee55820
prasadsourya <prasadsourya@gmail.com>
2020-07-15 18:10:53 +0530

    Jul 15  18:10:53

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -9,4 +9,5 @@ def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
 	a = math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
 	b = math.sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2))
-	c = math.sqrt((x1-x3)*(x1-x3)+(y1-y2)*(y1-y2))
+	c = math.sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3))
+	
