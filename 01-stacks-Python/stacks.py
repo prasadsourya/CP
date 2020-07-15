e8f70864f7ccf68afdcc4691e08493a92a064616
prasadsourya <prasadsourya@gmail.com>
2020-07-15 21:30:24 +0530

    Jul 15  21:30:24

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -43,9 +43,9 @@ class stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        Linkedlinsert_first(self,new_element)
+        LinkedList.insert_first(self,new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        self.ll.delete_first(self)
+        LinkedList.delete_first(self)
     
