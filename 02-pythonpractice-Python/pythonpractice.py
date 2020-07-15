1b2860f38edb50e443e02ea13b2964994026b2fa
prasadsourya <prasadsourya@gmail.com>
2020-07-15 13:20:18 +0530

    Jul 15  13:20:17

--- 02-pythonpractice-Python/pythonpractice.py
+++ 02-pythonpractice-Python/pythonpractice.py
@@ -19,7 +19,7 @@ class Classy(object):
     def __init__(self):
         self.items = []
     def fancy(self,x):
-        items = []
+        self.items = []
         items.append(x)
     def classiness(self,x):
         sum =0
