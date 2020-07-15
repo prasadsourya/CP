ce046edb906e4c5baa240fa1bc6d3b17ba72bdbe
prasadsourya <prasadsourya@gmail.com>
2020-07-15 18:03:53 +0530

    Jul 15  18:03:53

--- 14-islegaltriangle-Python/islegaltriangle.py
+++ 14-islegaltriangle-Python/islegaltriangle.py
@@ -4,7 +4,6 @@
 # otherwise. Note from the triangle inequality that the sum of each two sides must be greater
 # than the third side, and further note that all sides of a legal triangle must be positive. Hint:
 # how can you determine the longest side, and how might that help?
-import math
 def islegaltriangle(s1, s2, s3):
 	# your code goes here
 	if (s1>0 and s2>0 and s3>0 and s1+s2>s3 and s2+s3>s1 and s3+s1>s2):
