d5af62b3ef17ab017fac0721f7955a773516575b
prasadsourya <prasadsourya@gmail.com>
2020-07-15 18:27:28 +0530

    Jul 15  18:27:27

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,13 +7,13 @@
 import math
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	a = int(math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)))
-	b = int(math.sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2)))
+	a = math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+	b = math.sqrt((x3-x2)*(x3-x2)+(y3-y2)*(y3-y2))
 	c = math.sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3))
 	x=a*a
 	y=b*b
 	z=c*c
-	if ((x+y==z)or(y+z==x)or(x+z==y)):
+	if (math.isclose(x+y,z)or(math.isclose(y+z,x))or math.isclose(x+z==y)):
 		return True
 	else :
 		return False
