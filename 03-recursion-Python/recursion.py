cccfaf3ba829b376fdd0126d57dc186a738ae698
prasadsourya <prasadsourya@gmail.com>
2020-07-16 12:59:22 +0530

    Jul 16  12:59:21

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -7,4 +7,4 @@ def get_fib(position):
     if position<=1:
         return position
     else:
-        return (get_fib(position-1)+get_fib())
+        return (get_fib(position-1)+get_fib(position))
