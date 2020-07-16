8d0298e98e7f3cbc964e78b0519c4d94feaeee07
prasadsourya <prasadsourya@gmail.com>
2020-07-16 12:25:40 +0530

    Jul 16  12:25:39

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -26,12 +26,12 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        if self.head==None:
-            self.head=Element(new_element)
-        else :
-            new_part =Element(new_element)
-            new_part.next=self.head
-            self.head=new_part
+        # if self.head==None:
+        #     self.head=Element(new_element)
+        # else :
+        new_part =Element(new_element)
+        new_part.next=self.head
+        self.head=new_part
 
 
 
