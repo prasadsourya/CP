d491abef8a6ae672919e7183d06773f7df2bf9a3
prasadsourya <prasadsourya@gmail.com>
2020-07-17 12:58:17 +0530

    Jul 17  12:58:17

--- 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
+++ 11-bonusplaythreediceyahtzee-Python/bonusplaythreediceyahtzee.py
@@ -69,9 +69,13 @@ def playstep2(hand, dice):
 	arr = (k,m)
 	return arr
 
+def score(digit):
+	a=digit%10
+	b=digit
+
 def bonusplaythreediceyahtzee(dice):
 	# Your code goes here
 	new_dice=int(str((dice)%10)+str((dice/10)%10)+str((dice/100)%10))
 	hand = int(str((dice/1000000)%10)+str((dice/100000)%10)+str((dice/10000)%10)+str((dice/1000)%10))
 	x=playstep2(hand,new_dice)
-	
+
