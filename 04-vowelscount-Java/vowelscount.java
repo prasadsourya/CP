6d0cab3c60b85a56eb7e16cef4de8419dd759eb9
prasadsourya <prasadsourya@gmail.com>
2020-07-18 14:53:38 +0530

    Jul 18  14:53:38

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -6,8 +6,16 @@
 
 class vowelscount {
 	public int fun_vowelscount(String s){
-		// your code goes here
-		return 0;
+		int count=0;
+		for(int i=0;i<s.length();i++) {
+			if (s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
+				count++;
+			}
+			else if ((s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u')) {
+				count++;
+			}			
+		}
+		return count;
 	}
 	
 }
