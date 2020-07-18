b7ce74006a9c493dfda238e5463284086a783425
prasadsourya <prasadsourya@gmail.com>
2020-07-18 13:55:48 +0530

    Jul 18  13:55:48

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -14,7 +14,7 @@ def fun_applycaesarcipher(msg, shift):
 	msg1=""
 	for i in msg:
 		sum = ord(i)+shift
-		if(i==' '):
+		if(i):
 			msg=' '
 		elif (sum>90 and sum<97 and shift>0):
 			msg1=msg1+chr(sum-26)
