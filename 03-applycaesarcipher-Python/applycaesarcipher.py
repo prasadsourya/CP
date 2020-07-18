11649f56a05c895511c616e16bde34c3b941c184
prasadsourya <prasadsourya@gmail.com>
2020-07-18 14:01:31 +0530

    Jul 18  14:01:31

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -24,8 +24,6 @@ def fun_applycaesarcipher(msg, shift):
 			msg1=msg1+chr(sum+26)
 		elif (sum>122  and shift<0):
 			msg1=msg1+chr(sum+26)
-		elif (sum>=97  and shift<0):
-			msg1=msg1+chr(sum+26)
 		else:
 			msg1=msg1+chr(sum)
 	return msg1
