689f8574992a3e81b9e4b869857c4580efa8c36e
prasadsourya <prasadsourya@gmail.com>
2020-07-18 15:18:16 +0530

    Jul 18  15:18:15

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -18,7 +18,7 @@ def fun_interleave(s1,s2):
 	else:
 		for i in range(s_min):
 			result =result +s1[i]+s2[i]
-		for j in range(s_max-s_min-2,s_max-1):
+		for j in range(s_max-2*s_min,s_max):
 			if(len(s1)>len(s2)):
 				result = result+s1[j]
 			else:
