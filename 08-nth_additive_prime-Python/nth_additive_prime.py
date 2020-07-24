92798ec895ddd455b545e043d774d3e7c8b81a8a
prasadsourya <prasadsourya@gmail.com>
2020-07-24 12:10:12 +0530

    Jul 24  12:10:12

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -2,7 +2,13 @@
 # and returns the nth Additive Prime, which is a prime number such that 
 # the sum of its digits is also prime. For example, 113 is prime and 1+1+3==5 and 5 
 # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
-
+def is_prime(n):
+	if n<=1:
+		return False
+	for i in range(2,m):
+		if (n%i==0):
+			return False
+	return True
 
 
 
