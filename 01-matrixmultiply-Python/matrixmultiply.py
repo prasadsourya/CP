cbc8adb55b5926db347229d1ce29bf5828f1fa55
prasadsourya <prasadsourya@gmail.com>
2020-07-24 22:29:34 +0530

    Jul 24  22:29:34

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -7,7 +7,11 @@
 def fun_matrixmultiply(m1, m2):
     if len(m1[0])!=len(m2):
         return None
-    result=[[0 for x in range(len(m2[0]))] for y in range(len(m2))]
+    result=[[0 for x in range(len(m2[0]))] for y in range(len(m1))]
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                result[i][j]+= m1[i][k]*m2[k][j]
 
 
 
