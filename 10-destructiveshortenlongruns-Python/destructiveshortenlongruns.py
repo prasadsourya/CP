0fb8af1f4b29f20d99c5e7681572602ad5f167fb
prasadsourya <prasadsourya@gmail.com>
2020-07-24 14:47:33 +0530

    Jul 24  14:47:32

--- 10-destructiveshortenlongruns-Python/destructiveshortenlongruns.py
+++ 10-destructiveshortenlongruns-Python/destructiveshortenlongruns.py
@@ -15,4 +15,12 @@
 def destructiveshortenlongruns(L, k):
 	# Your code goes here
 	l=[]
-	
+	for i in range(len(L)):
+		x=i+k
+		if x>len(L):
+			z=x-len(L)
+			x=x-z
+		for j in range(i,x):
+			if L[j]!=L[i]:
+				l.append(L[i])
+				break
